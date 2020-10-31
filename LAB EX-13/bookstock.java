b7e5b0effd467cd1a3854b4be829ed32ca541ed0
riobharath <69112425+riobharath@users.noreply.github.com>
2020-10-31 22:17:46 +0530

    Add files via upload

---
+++ LAB EX-13/bookstock.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.lab13;
+
+public class bookstock {
+
+    String title;
+    String author;
+    String publisher;
+    double price;
+    int stock;
+     bookstock(String t,String a,String p,double pri,int s){
+     title=t;
+     author=a;
+     publisher=p;
+     price=pri;
+     stock=s;
+     
+     }
+     
+    
+    
+}
